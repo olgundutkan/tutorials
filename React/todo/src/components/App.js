76e0c8b4ba3d74233733ef9d058b59784c36ce96
olgundutkan <olgundutkan@gmail.com>
2018-05-27 21:48:14 +0300

    Added react app

---
+++ React/todo/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import '../styles/App.css';
+import From from "../containers/Form";
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <From></From>
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ React/todo/src/containers/Form/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import FullWidthGrid from '../../components/FullWidthGrid';
+
+export default class From extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <FullWidthGrid>
+                <div>TEST</div>
+            </FullWidthGrid>
+        )
+    }
+
+}
